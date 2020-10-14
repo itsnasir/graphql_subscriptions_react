1b5649cacff689435e3bf0939edbf5f5936ef450
Nasir Ibrahim <itsnasir@gmail.com>
2020-10-14 17:08:56 -0400

    [HOLR-1493] Append subscriptions response

--- src/liveStreamings.js
+++ src/liveStreamings.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react';
 import LiveStreaming from './liveStreaming'
 import { useQuery, gql } from '@apollo/client';
 
@@ -11,22 +11,34 @@ query{
 }
 `
 const NEW_LIVE_STREAMINGS = gql`
-  subscription {
-    newLink {
-      id
-      url
-      description
-    }
+subscription{
+  newStreamingLink {
+    id
+    url
   }
+}
 `
 
 const LiveStreamings=(props) => {
   const { loading, error, data, subscribeToMore } = useQuery(LIVE_STREAMINGS);
-  console.log(subscribeToMore)
-  const getLiveStreamings=() => {
 
+  subscribeToMore({
+    document: NEW_LIVE_STREAMINGS,
+    updateQuery(prev, { subscriptionData }) {
+      console.log("ss =>", subscriptionData)
+       if (!subscriptionData.data) return prev
+       console.log("ss =>", subscriptionData.data)
+
+       return {
+         liveStreamings: [...prev.liveStreamings, subscriptionData.data.newStreamingLink]
+       }
+    }
+  });
+
+  const getLiveStreamings=() => {
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
+
     const linksToRender = data.liveStreamings
     return (
       <div>
